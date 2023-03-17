59f86cfc80c81757c50a19f524f5882095421fab
Nunes-Will <nuneswillroberto@gmail.com>
2023-03-17 14:20:32 -0300

    Add autor no adimin

--- livraria/admin.py
+++ livraria/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Categoria,Editora
+from .models import Categoria,Editora,Autor
 
 admin.site.register(Categoria)
 admin.site.register(Editora)
+admin.site.register(Autor)
--- livraria/models.py
+++ livraria/models.py
@@ -24,3 +24,13 @@ class Autor(models.Model):
 
         class Meta:
             Verbose_name_plural("Autores")
+
+
+# class Livros (models.Model):
+#     titulo = models.CharField (max_length = 255)
+#     isbn = models.CharField (max_length = 32, null = True, blank = True)
+#     qtd = models.IntegerField (default = 0, null = True, blank = True)
+#     preco = models.DecimalField (max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+
+#     def __str__(self):
+#         return f"{self.titulo} ({self.qtd})"
