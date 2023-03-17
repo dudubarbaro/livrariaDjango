3a93cef30db4ca893367e99099d29a9e7fb98364
Nunes-Will <nuneswillroberto@gmail.com>
2023-03-17 14:11:50 -0300

    Add Autor, senha e usar mudados

--- livraria/models.py
+++ livraria/models.py
@@ -13,3 +13,14 @@ class Editora(models.Model):
 
     def __str__(self):
         return self.nome
+
+
+class Autor(models.Model):
+     nome_autor = models.CharField(max_length=255)
+     email_autor = models.EmailField ( null= True, blank =True )
+
+     def __str__(self):
+        return self.nome_autor
+
+        class Meta:
+            Verbose_name_plural("Autores")
