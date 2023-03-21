7f14906e49273f2ab56371ecb6c1d0a4058b63b1
Nunes-Will <nuneswillroberto@gmail.com>
2023-03-21 14:55:13 -0300

    Tudo rodando

--- livraria/models.py
+++ livraria/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
 
@@ -16,21 +17,26 @@ class Editora(models.Model):
 
 
 class Autor(models.Model):
-     nome_autor = models.CharField(max_length=255)
-     email_autor = models.EmailField ( null= True, blank =True )
+    nome_autor = models.CharField(max_length=255)
+    email_autor = models.EmailField(null=True, blank=True)
 
-     def __str__(self):
+    def __str__(self):
         return self.nome_autor
-
         class Meta:
-            Verbose_name_plural("Autores")
+            verbose_name_plural = "Autores"
 
 
 class Livro (models.Model):
-    titulo = models.CharField (max_length = 255)
-    isbn = models.CharField (max_length = 32, null = True, blank = True)
-    qtd = models.IntegerField (default = 0, null = True, blank = True)
-    preco = models.DecimalField (max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+    titulo = models.CharField(max_length=255)
+    isbn = models.CharField(max_length=32, null=True, blank=True)
+    qtd = models.IntegerField(default=0, null=True, blank=True,)
+    preco = models.DecimalField(
+        max_digits=7, decimal_places=2, default=0, null=True, blank=True)
+
+    categoria = models.ForeignKey(
+        Categoria, on_delete=models.PROTECT, related_name="livros")
+    editora = models.ForeignKey(
+        Editora, on_delete=models.PROTECT, related_name="livros")
 
     def __str__(self):
         return f"{self.titulo} ({self.qtd})"
