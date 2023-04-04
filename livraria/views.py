2ff8ab8fa7302dc8e136245769d5bbf46f86785c
Nunes-Will <nuneswillroberto@gmail.com>
2023-04-04 14:19:14 -0300

    add djangorestfram., serializers.py e ViewSet

--- livraria/views.py
+++ livraria/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.viewsets import ModelVielSet
+
+from livraria.models import Categoria
+from livraria.serializers import CategoriaSerializers
+
+class CategoriaSerializers(ModelVielSet):
+    queryset = Categoria.objects.all()
+    serializers_class = CategoriaSerializers
