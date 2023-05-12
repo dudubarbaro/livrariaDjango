62efaef2ecdbdff669ba215e0f2eaadca513288e
Nunes-Will <nuneswillroberto@gmail.com>
2023-05-12 14:22:54 -0300

    Arrumando escrita

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheader',
+    'corsheaders',
     'rest_framework',
     "livraria",
 ]
@@ -123,5 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 CORS_ALLOW_ALL_ORIGINS = True
