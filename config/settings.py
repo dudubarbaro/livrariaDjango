64457422b7eec1cc5adbbb72375e6e0e07478fdc
Nunes-Will <nuneswillroberto@gmail.com>
2023-06-27 14:12:26 -0300

    Settings comentario no final

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,6 @@ REST_FRAMEWORK = {
     # "DEFAULT_PERMISSION_CLASSES": [
     #     "rest_framework.permissions.DjangoModelPermissions",],
 
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework_simplejwt.authentication.JWTAuthentication",),
+    # "DEFAULT_AUTHENTICATION_CLASSES": (
+    #     "rest_framework_simplejwt.authentication.JWTAuthentication",),
 }
